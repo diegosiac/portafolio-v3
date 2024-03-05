3453dc19d269454f42b9440a024742afe8055b1b
diegosiac <diego35502@hotmail.com>
2024-03-05 03:23:08 -0600

    Add tailwind.config.mjs file with customizations

---
+++ tailwind.config.mjs
@@ -0,0 +1,46 @@
+import animations from "@midudev/tailwind-animations"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				orange: "var(--color-orange)",
+				green: "var(--color-green)",
+			},
+			backgroundColor: {
+				"primary-blur": "var(--color-primary-blur)",
+				"secondary-blur": "var(--color-secondary-blur)",
+			},
+			textColor: {
+				"primary-inactive": "var(--color-primary-inactive)",
+			},
+			animation: {
+				spin: "ping2 1s cubic-bezier(0, 0, 0.2, 1) infinite",
+			},
+			boxShadowColor: {
+				glow: "var(--box-shadow-glows)",
+			},
+			keyframes: {
+				ping2: {
+					"75%, 100%": { transform: "scale(2)", opacity: 0 },
+					"100%": { transform: "rotate(360deg)" },
+				},
+				fadeIn: {
+					"0%": { opacity: "0" },
+					"100%": { opacity: "1" },
+				},
+			},
+		},
+	},
+	darkMode: "class",
+	plugins: [
+		animations,
+		function ({ addVariant }) {
+			addVariant("any-hover", "@media (any-hover: hover) { &:hover }")
+		},
+	],
+}
