11978f07bc78dcbfbfc8cba9d8130044be2c9845
diegosiac <diego35502@hotmail.com>
2024-03-05 04:57:22 -0600

    Add experience data and export it

---
+++ src/data/experience.ts
@@ -0,0 +1,12 @@
+import type { Experience } from "@/interfaces";
+
+export const experience: Experience[] = [
+	{
+		company: "Omica",
+		position: "Frontend Developer",
+		period: "2021 - Present",
+		description:
+			"I'm working on the development of the company's website and the development of the company's products.",
+		isLast: true,
+	},
+]
