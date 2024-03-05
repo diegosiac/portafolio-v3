eace3ce5a7a0a5b1f72276c1cdd12f1c6f6d29bd
diegosiac <diego35502@hotmail.com>
2024-03-05 05:07:52 -0600

    Update period in experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,7 +4,7 @@ export const experience: Experience[] = [
 	{
 		company: "Omica",
 		position: "Frontend Developer",
-		period: "2021 - Present",
+		period: "2023 - Actualidad",
 		description:
 			"I'm working on the development of the company's website and the development of the company's products.",
 		isLast: true,
