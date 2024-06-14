27adc70077edae2af68edc8ebe3f29bcb75b4349
diegosiac <diego35502@hotmail.com>
2024-06-13 20:27:18 -0600

    refactor: Update Astro configuration and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
+import preact from "@astrojs/preact"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
-  output: "server",
-  adapter: vercel(),
-});
+	integrations: [tailwind(), preact({ compat: true })],
+	output: "server",
+	adapter: vercel(),
+})
