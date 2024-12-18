82f3bc8b3119841380e2a6f4874250f67e39db3a
diegosiac <diego35502@hotmail.com>
2024-12-17 20:05:05 -0600

    add TextRevealByWord component for animated word reveal on scroll, enhancing text visibility and interaction in UI

--- src/components/ui/text-reveal.tsx
+++ src/components/ui/text-reveal.tsx
@@ -19,16 +19,16 @@ export const TextRevealByWord = ({
 	const words = text.split(" ")
 
 	return (
-		<div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
+		<div ref={targetRef} className={cn("relative z-0 h-[200vh] w-full", className)}>
 			<div
 				className={
-					"sticky top-0 mx-auto flex h-[50%] max-w-4xl items-center bg-transparent px-[1rem] py-[5rem]"
+					"sticky top-0 mx-auto flex h-[50%] items-center bg-transparent px-[1rem] py-[5rem]"
 				}
 			>
 				<p
 					ref={targetRef}
 					className={
-						"flex flex-wrap p-5 text-2xl font-bold text-primary/30 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl"
+						"flex flex-wrap p-5 text-2xl font-bold text-primary/30 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl text-balance"
 					}
 				>
 					{words.map((word, i) => {
@@ -63,5 +63,3 @@ const Word = ({ children, progress, range }: WordProps) => {
 		</span>
 	)
 }
-
-export default TextRevealByWord
