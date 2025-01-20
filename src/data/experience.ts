f64bfe27893a041d6cfc97335bdb8f853bde9742
Diego Cruz <diego35502@hotmail.com>
2025-01-20 06:45:14 -0600

    Update experience entries in src/data/experience.ts; refine job periods and descriptions for clarity and completeness

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,10 +5,16 @@ export const experience: Experience[] = [
 		urlCompany: "https://www.omica.com.ar/",
 		company: "Omica",
 		position: "Frontend Developer",
-		startPeriod: "Oct 2023",
-		endPeriod: "Actualidad",
+		period: "Oct 2023 - Presente",
 		description:
-			"I'm working on the development of the company's website and the development of the company's products.",
-		isLast: true,
+			"Colaboré con un equipo multidisciplinario para desarrollar soluciones que optimizaron la experiencia del usuario y alinearon los productos con los objetivos estratégicos. Implementé procesos de integración continua y despliegue que mejoraron la eficiencia operativa, reduciendo los tiempos de entrega en un 30%",
+	},
+	{
+		urlCompany: "",
+		company: "Freelance",
+		position: "Desarrollador Fullstack",
+		period: "May 2023 - Agt 2023",
+		description:
+			"Me dediqué a desarrollar soluciones web y mobile para clientes de diferentes industrias, desde la industria automotriz hasta la de la educación. Me especializé en React Native y React, y me dediqué a crear aplicaciones de alta calidad y de alta experiencia.",
 	},
 ]
